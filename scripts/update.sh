dc68e442c305dad500dda6df1d22129be4b4a752
bianqu <bianqu@f5.si>
2021-03-24 19:51:34 +0800

    fix: 修改update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,28 +1,9 @@
 #! /usr/bin/env bash
 
-# 更新jd_scripts
-
-# npm i git+https://gitee.com/zixing/jd_scripts.git#master
-# SCRIPTS_GIT_URL=git+https://gitee.com/zixing/jd_scripts.git#master
-
-ROOT_DIR=$(pwd)
-echo "ROOT_DIR >> $ROOT_DIR"
-NODE_MODULES_DIR=${ROOT_DIR}/node_modules
-if [ ! -e "$NODE_MODULES_DIR" ]
-then
-  mkdir "$NODE_MODULES_DIR"
-fi
-
-# jd_scripts项目路径
-SCRIPTS_PACKAGE_NAME=LXK9301
-SCRIPTS_WORKSPACE=${NODE_MODULES_DIR}/${SCRIPTS_PACKAGE_NAME}
-
-rm ~/.ssh/jd_id_rsa
-cp ./jd_id_rsa ~/.ssh/jd_id_rsa
-chmod 600 ~/.ssh/jd_id_rsa
-
-export GIT_SSH_COMMAND='ssh -i ~/.ssh/jd_id_rsa'
-
-
-npm install --no-save git+ssh://git@gitee.com:lxk0301/jd_scripts.git
-cp ./jdCookie.js "$SCRIPTS_WORKSPACE"/jdCookie.js
+cd /scripts
+git remote set-url origin "$REPO_URL"
+git reset --hard
+echo "git pull拉取最新代码..."
+git -C /scripts pull --rebase
+echo "npm install 安装最新依赖"
+npm install --prefix /scripts
